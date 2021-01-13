0229181d11cb5f31b69fc72d8679622c3ca29c74
Gaurav soni <77190167+GauravGaurav757@users.noreply.github.com>
2021-01-13 19:40:16 +0530

    Add files via upload
    In this file we are able to find SI of any group of numbers.

---
+++ si formula.py
@@ -0,0 +1,19 @@
+"""
+This programme will 
+find the s.i of your number
+
+"""
+print("Don't put Rupees sign in this.")
+a=(input("Enter your Principle no.:"))
+
+print("\nDon't put percent sign.")
+b=(input("Enter your Rate percent:"))
+
+print("\nIn time don't put Y value for year.")
+c=(input("Enter the time:"))
+
+d=float(a)*float(b)*float(c)//100 #here the simple interest formula will take place
+
+print("\nHere is the S.I of yours",d)
+
+#Now the programme is ready to use
