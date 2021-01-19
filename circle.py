91d5ded8763f01228772d60e435fe4072ac5181d
Gaurav soni <77190167+GauravGaurav757@users.noreply.github.com>
2021-01-19 15:43:36 +0530

    This file will find the Circle area.

---
+++ circle.py
@@ -0,0 +1,15 @@
+"""
+This programme will
+find you area of circle.
+
+"""
+
+print("Find the 'Area of your circle'")
+
+a= input("Enter your radius:")
+
+b= 3.14*float(a)**2 #Here the area will be found 
+
+print("Here is your Area:",b)#Here your output take place.
+
+#Done
