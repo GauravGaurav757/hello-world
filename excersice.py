0451760281c174bae28a385644e88638b9761d31
Gaurav soni <77190167+GauravGaurav757@users.noreply.github.com>
2021-03-20 14:28:22 +0530

    Sort.py
    This will sort the number you write and the number can be very large also.

---
+++ excersice.py
@@ -0,0 +1,9 @@
+"""
+items = ['Watch','Laptop','Ipad','earbuds','Headphones']
+items.reverse()
+print(items)
+"""
+number = []
+number = list(map(int,input("Enter the numbers : ").strip().split(',')))
+number.sort()
+print(number)
